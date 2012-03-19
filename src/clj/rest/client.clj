84c768ddb74d2392441d5f29672dbb0f8d04f66d
Roman <roman@burningswell.com>
2012-03-19 21:39:01 +0100

    Added tests for client ns.

--- src/clj/rest/client.clj
+++ src/clj/rest/client.clj
@@ -8,7 +8,7 @@
 (defprotocol IRequest
   (request [arg] "Convert `arg` to Ring request map."))
 
-(defn build-request
+(defn make-request
   "Send the HTTP request via *client*."
   [request-method resource & {:as options}]
   (-> (merge (request resource) options)
@@ -21,37 +21,37 @@
 (defn delete
   "Get the represention of `resource`."
   [resource & options]
-  (send-request (apply build-request :delete resource options)))
+  (send-request (apply make-request :delete resource options)))
 
 (defn get
   "Get the represention of `resource`."
   [resource & options]
-  (send-request (apply build-request :get resource options)))
+  (send-request (apply make-request :get resource options)))
 
 (defn head
   "Check if the `resource` exists."
   [resource & options]
-  (send-request (apply build-request :head resource options)))
+  (send-request (apply make-request :head resource options)))
 
 (defn options
   "Get the options of `resource`."
   [resource & options]
-  (send-request (apply build-request :options resource options)))
+  (send-request (apply make-request :options resource options)))
 
 (defn post
   "Create a new `resource`."
   [resource & options]
-  (send-request (apply build-request :post resource options)))
+  (send-request (apply make-request :post resource options)))
 
 (defn put
   "Change the representation of `resource`."
   [resource & options]
-  (send-request (apply build-request :put resource options)))
+  (send-request (apply make-request :put resource options)))
 
 (defn trace
   "Trace the `resource`."
   [resource & options]
-  (send-request (apply build-request :trace resource options)))
+  (send-request (apply make-request :trace resource options)))
 
 (defmacro with-client
   "Evaluate `body` with *client* bound to `client`."
@@ -66,10 +66,10 @@
 
 (extend-type clojure.lang.IPersistentMap
   IRequest
-  (request [{:keys [uri url] :as resource}]
+  (request [{:keys [server-name uri url] :as resource}]
     (cond
-     (string? uri)
+     (and (string? server-name) (string? uri))
      resource
-     (and (string? url)
-          (re-matches #"https?://.*" url))
-     (assoc (parse-url url) :body resource))))
+     (string? url)
+     (assoc (parse-url url) :body resource)
+     :else (throw (Exception. (str "Can't create Ring request map from: " resource))))))
--- test/clj/rest/test/client.clj
+++ test/clj/rest/test/client.clj
@@ -1,4 +1,5 @@
 (ns rest.test.client
+  (:import java.net.MalformedURLException)
   (:refer-clojure :exclude (get))
   (:use clojure.test
         rest.client))
@@ -128,3 +129,15 @@
        (is (= "example.com" (:server-name request)))
        (is (= "/users/1-roman" (:uri request))))]
     (trace {:url "http://example.com/users/1-roman"})))
+
+(deftest test-request
+  (is (thrown? MalformedURLException (request "")))
+  (is (thrown? MalformedURLException (request "x")))
+  (let [request (request "http://example.com/")]
+    (is (= :http (:scheme request)))
+    (is (= "example.com" (:server-name request)))
+    (is (= "/" (:uri request))))
+  (let [request (request {:url "http://example.com/"})]
+    (is (= :http (:scheme request)))
+    (is (= "example.com" (:server-name request)))
+    (is (= "/" (:uri request)))))
