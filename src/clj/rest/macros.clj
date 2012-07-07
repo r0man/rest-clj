f944aa86698ff838e96c745ea1b3756973a576c4
Roman <roman@burningswell.com>
2012-07-07 10:41:35 +0200

    More cleanup.

--- src/clj/rest/macros.clj
+++ src/clj/rest/macros.clj
@@ -3,9 +3,8 @@
   (:refer-clojure :exclude [replace])
   (:require [clojure.string :refer [upper-case replace]]
             [inflections.core :refer [singular plural]]
-            [routes.core :as routes]
-            ;; rest.client
-            ))
+            [rest.client :refer [send-request]]
+            [routes.core :refer [defroute]]))
 
 (defmacro defresources [name args pattern & {:as options}]
   (let [name# name
@@ -13,30 +12,30 @@
         pattern# pattern
         singular# (singular name)
         ns# *ns*]
-    `(do (routes/defroute ~name# [~@(reverse (rest (reverse args#)))]
+    `(do (defroute ~name# [~@(reverse (rest (reverse args#)))]
            ~(replace pattern# #"/[^/]+$" ""))
-         (routes/defroute ~singular# [~@args#]
+         (defroute ~singular# [~@args#]
            ~pattern#)
-         (routes/defroute ~(symbol (str "new-" singular#)) []
+         (defroute ~(symbol (str "new-" singular#)) []
            ~(str (replace pattern# #"/[^/]+$" "") "/new"))
-         (routes/defroute ~(symbol (str "edit-" singular#)) [~@args#]
+         (defroute ~(symbol (str "edit-" singular#)) [~@args#]
            ~(str pattern# "/edit"))
          (defn ~name# [& {:as ~'opts}]
-           (~'rest.client/send-request
+           (rest.client/send-request
             (~(symbol (str ns# "/" name# "-url")))))
          (defn ~singular# [~singular# & {:as ~'opts}]
-           (~'rest.client/send-request
+           (rest.client/send-request
             (~(symbol (str ns# "/" singular# "-url")) ~singular#) ~'opts))
          (defn ~(symbol (str "create-" singular#)) [~singular# & {:as ~'opts}]
-           (~'rest.client/send-request
+           (rest.client/send-request
             (~(symbol (str ns# "/" name# "-url")))
             (merge {:method :post :body ~singular#} ~'opts)))
          (defn ~(symbol (str "delete-" singular#)) [~singular# & {:as ~'opts}]
-           (~'rest.client/send-request
+           (rest.client/send-request
             (~(symbol (str ns# "/" singular# "-url")) ~singular#)
             (merge {:method :delete} ~'opts)))
          (defn ~(symbol (str "update-" singular#)) [~singular# & {:as ~'opts}]
-           (~'rest.client/send-request
+           (rest.client/send-request
             (~(symbol (str ns# "/" singular# "-url")) ~singular#)
             (merge {:method :put :body ~singular#} ~'opts))))))
 
@@ -50,5 +49,5 @@
 (defmacro with-server
   "Evaluate `body` with *server* bound to `server`."
   [server & body]
-  `(routes/with-server ~server
+  `(routes.core/with-server ~server
      ~@body))
