36ecd96a1e19906dc8c70e8ddf83dacabc7150fa
Roman Scherer <roman.scherer@burningswell.com>
2012-08-10 12:13:50 +0200

    Better clojurescript test script.

--- test-cljs.sh
+++ test-cljs.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-echo "rest.test.run()" | d8 --shell target/rest-test.js
+
+JS_TESTS="target/rest-test.js"
+
+[ ! -f $JS_TESTS ] && lein cljsbuild once
+
+if [ -n "`type d8 2> /dev/null`" ] ; then
+    echo "rest.test.run()" | d8 --shell $JS_TESTS
+elif [ -n "`type v8 2> /dev/null`" ] ; then
+    echo "rest.test.run()" | v8 --shell $JS_TESTS
+else
+    echo "Can't run ClojureScript tests. Looks like V8 is not installed."
+    exit 1
+fi
