15259f34af381ff5e04b50416873de85e5dd396a
Roman Scherer <roman.scherer@burningswell.com>
2013-04-19 10:46:56 +0200

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject rest-clj "0.0.2-SNAPSHOT"
+(defproject rest-clj "0.0.3-SNAPSHOT"
   :description "A HTTP REST library for Clojure."
   :url "http://github.com/r0man/rest-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0"
-  :dependencies [[clj-http "0.7.0"]
+  :dependencies [[clj-http "0.7.2"]
                  [clj-stacktrace "0.2.5"]
-                 [cljs-http "0.0.2-SNAPSHOT"]
-                 [org.clojure/clojure "1.5.0"]
-                 [routes-clj "0.0.2-SNAPSHOT"]
+                 [cljs-http "0.0.4-SNAPSHOT"]
+                 [org.clojure/clojure "1.5.1"]
+                 [routes-clj "0.0.3-SNAPSHOT"]
                  [slingshot "0.10.3"]]
   :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.3"]]}}
   :plugins [[lein-cljsbuild "0.3.0"]]
