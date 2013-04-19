1090e76dae32d8c50ac0501412d693133cd17f73
Roman Scherer <roman.scherer@burningswell.com>
2013-04-19 17:27:18 +0200

    Bumped versions.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rest-clj "0.0.3-SNAPSHOT"
+(defproject rest-clj "0.0.4-SNAPSHOT"
   :description "A HTTP REST library for Clojure."
   :url "http://github.com/r0man/rest-clj"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                  [clj-stacktrace "0.2.5"]
                  [cljs-http "0.0.4-SNAPSHOT"]
                  [org.clojure/clojure "1.5.1"]
-                 [routes-clj "0.0.3-SNAPSHOT"]
+                 [routes-clj "0.0.4-SNAPSHOT"]
                  [slingshot "0.10.3"]]
   :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.3"]]}}
   :plugins [[lein-cljsbuild "0.3.0"]]
